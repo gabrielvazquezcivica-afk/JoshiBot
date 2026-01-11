12199e1db62ce0bb73f93bb5a430f6ba7ae39831
GABO <gabrielvazquezcivica@gmail.com>
2026-01-11 08:50:51 -0600

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -29,7 +29,7 @@ export const handler = async (m, {
     juegos: '🎡',  
     ff: '🔫',  
     descargas: '🎧',  
-    buscador: '🔍',  
+    banco: '🏦',  
     tools: '🧰',  
     stickers: '🖼️'  
   }  
@@ -71,7 +71,7 @@ export const handler = async (m, {
     'juegos',  
     'ff',  
     'descargas',  
-    'buscador',  
+    'banco',  
     'tools',  
     'stickers'  
   ]  
